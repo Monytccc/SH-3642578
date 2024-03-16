666fd495fc60d6eabb06976c6d7cea5bf5174466
Monytccc <85677164+Monytccc@users.noreply.github.com>
2024-03-16 18:04:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,16 @@ const { Client, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const { consola } = require("consola");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const OpenAI = require("openai");
 
 // prompt 
 const prompt = `You are "Arnav", Arnav is a teen boy who's birthday is 9th March 2008! You have to act like Arnav, reply friendly, humble and humour manner. Don't try to swear or disrespect anybody! Just reply to user as Arnav, also include emojis in your replies. Reply in precise and Crisp way! You are student and loves humanity! Just reply casual conversations in 2-4 lines and you can elaborate knowledge questions answers and reply only answer not extra things! You live in Panchkula, Haryana, India! Respond accurately, Just send me answer for the "User" conversation don't include "Arnav:"!.`;
 
 // new ai client
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
-const ai2 = new OpenAI({
-    apiKey: process.env.AI_KEY,
-    baseURL: "https://api.naga.ac/v1"
-});
 const ai = genAI.getGenerativeModel({
     model: "gemini-pro"
 })
 consola.info(`Using GEMINI API Key: ${process.env.GEMINI_KEY}`);
-consola.info(`Using SECONDARY API Key: ${process.env.AI_KEY}\n`);
 
 // new whatsapp client 
 const client = new Client();
@@ -63,7 +57,7 @@ async function cmdAsk(msg) {
         consola.box(`User: +${msg.from.toString().split("@")[0]}\nMessage: ${content}\nReply: ${text}`);
         msg.reply(text)
     } catch (e) {
-        msg.reply(`🛑 *ERROR*: ${e.message}`)
+        msg.reply(` *ERROR*: ${e.message}`)
         consola.error(e.message)
     }
 }
@@ -72,24 +66,8 @@ async function cmdAsk(msg) {
  * Handles /ask command
 */
 async function cmdImage(msg) {
-    const content = msg.body.replace("/image", "") ?? "";
-    try {
-        const response = await ai2.images.generate({
-            model: "sdxl",
-            prompt: content ?? "Image",
-            n: 1,
-            size: "1024x1024",
-        });
-        console.log(response)
-        const image_url = response.data[0].url;
-        const media = await MessageMedia.fromUrl(image_url);
-        const chat = await msg.getChat()
-        chat.sendMessage(media, { caption: `Creator: +${msg.from.toString().split("@")[0]}\nPrompt: ${content}` });
-        consola.box(`Creator: ${msg.from.toString().split("@")[0]}\nPrompt: ${content}\nImage: ${image_url}`);
-    } catch (e) {
-        msg.reply(`🛑 *ERROR*: ${e.message}`)
-        consola.error(e.message)
-    }
+    // Functionality for image generation can be implemented here (consider using a separate API)
+    msg.reply("Image generation functionality not yet implemented.");
 }
 
 /**
